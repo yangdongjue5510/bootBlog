9067fd08f537140390d67d20e33c2002aa558e8f
yangdongjue5510 <djwhy5510@gamil.com>
2021-08-26 15:27:35 +0900

    fix: start new project

--- blog/src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BlogControllerTest {
-        @GetMapping("/test/hello")
-        public String hello() {
-            return "<h1>hello spring boot</h1>";
-        }
+    @GetMapping("/test/hello")
+    public String hello() {
+        return "<h1>hello spring boot</h1>";
+    }
 }
